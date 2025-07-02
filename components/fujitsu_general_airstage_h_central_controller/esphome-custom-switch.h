b10f5c4c69562203450776fc75153296740f5cd2
Omni Flux <omniflux@omniflux.com>
2025-07-02 17:26:01 -0600

    Set custom switch state correctly

--- components/fujitsu_general_airstage_h_central_controller/esphome-custom-switch.h
+++ components/fujitsu_general_airstage_h_central_controller/esphome-custom-switch.h
@@ -10,7 +10,7 @@ namespace esphome::custom {
 class CustomSwitch : public Component, public switch_::Switch {
     public:
         CustomSwitch(std::function<bool(bool)> func) : func(func) {};
-        void write_state(bool state) override { this->publish_state(this->func(state)); };
+        void write_state(bool state) override { this->publish_state(this->func(state) ? state : this->state); };
 
     private:
         CustomSwitch() {};
